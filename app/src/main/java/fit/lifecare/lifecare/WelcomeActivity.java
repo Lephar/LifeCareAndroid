4ab9da2af2ce440affca925f0931896e0adf6330
Ali Emre Gülcü <aliemre.sirius@gmail.com>
2019-07-15 19:36:31 +0300

    Refactored WelcomeActivity

--- app/src/main/java/fit/lifecare/lifecare/WelcomeActivity.java
+++ app/src/main/java/fit/lifecare/lifecare/WelcomeActivity.java
@@ -1,133 +1,37 @@
 package fit.lifecare.lifecare;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Locale;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-    //Layout views
-    private ImageView login_button;
-    private TextView signup_button;
-
-    //firebase instance variables
-    private FirebaseAuth mAuth;
-    
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        //initialize views
-        login_button = findViewById(R.id.login_button);
-        signup_button = findViewById(R.id.signup_button);
-    
-        //initialize shared preferences
-        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        editor = preferences.edit();
+        Button login_button = findViewById(R.id.login_button);
+        Button signup_button = findViewById(R.id.signup_button);
 
-        //initalize Firebase components
-        mAuth = FirebaseAuth.getInstance();
-
-        checkThings();
-    }
-
-    private void checkThings(){
-        
-        String selected_lang = preferences.getString("lang","");
-        
-        if(selected_lang.equals("tr")){
-            setAppLocale(selected_lang);
-        } else if(selected_lang.equals("en")) {
-            setAppLocale(selected_lang);
-        }
-        
-        if (checkInternet()) {
-            checkUser();
-            initializeViewListeners();
-        } else {
-            AlertDialog.Builder builder;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
-            } else {
-                builder = new AlertDialog.Builder(this);
-            }
-            builder.setMessage(getString(R.string.sure_internet_connection))
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            //do nothing
-                            checkUser();
-                            initializeViewListeners();
-                        }
-                    }).show();
-            delay1sec();
-        }
-    }
-
-    private void delay1sec() {
-        Handler mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //start your activity here
-            }
-        }, 5000L);
-    }
-
-    private boolean checkInternet() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-
-        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void checkUser() {
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-
-            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }
-    }
-
-
-    private void initializeViewListeners() {
-        
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
@@ -136,25 +40,47 @@ public class WelcomeActivity extends AppCompatActivity {
         signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(WelcomeActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
 
+        prepareLocale();
+
+        if (checkConnection()) {
+            if (checkUser()) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+
+            login_button.setEnabled(true);
+            signup_button.setEnabled(true);
+        }
     }
-    
-    private void setAppLocale(String localeCode) {
+
+    private void prepareLocale() {
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration conf = res.getConfiguration();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            conf.setLocale(new Locale(localeCode.toLowerCase()));
-        } else {
-            conf.locale = new Locale(localeCode.toLowerCase());
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        conf.setLocale(new Locale(prefs.getString("lang", "en").toLowerCase()));
+        res.updateConfiguration(conf, dm);
+    }
+
+    private boolean checkConnection() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm != null) {
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            return netInfo != null && netInfo.isConnected();
         }
-        
-        res.updateConfiguration(conf,dm);
-    
+
+        return false;
+    }
+
+    private boolean checkUser() {
+        return FirebaseAuth.getInstance().getCurrentUser() != null;
     }
 }
